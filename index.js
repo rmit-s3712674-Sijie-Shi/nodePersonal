949ca3910b7684772814c557171ca5a953b0d57e
unknown <rikusrocks@gmail.com>
2020-10-19 03:09:37 +1100

    port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.static('build'))
-const PORT = 3005
+const PORT = process.env.PORT||3005
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
