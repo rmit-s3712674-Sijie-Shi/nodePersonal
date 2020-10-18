bed3250b8e2d83bf64eb58af6084438375c94ebd
unknown <rikusrocks@gmail.com>
2020-10-19 08:14:59 +1100

    1.0.2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.906671b6d8cdcf9d61c34f6abd92cd5f.js"
+  "/precache-manifest.ab8f27fbb0ce5a22b890f7a4dd20392b.js"
 );
 
 self.addEventListener('message', (event) => {
